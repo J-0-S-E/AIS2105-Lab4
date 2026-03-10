c9bf6c0d978790cdc1a6ea16c4ba7778682e188b
kristian <kristian.lokkeberg@gmail.com>
2026-03-10 10:35:09 +0100

    Oppgave 3 - del A&B

--- src/camera_pipeline/launch/pipeline.launch.py
+++ src/camera_pipeline/launch/pipeline.launch.py
@@ -10,6 +10,29 @@ def generate_launch_description():
         output='screen'
     )
 
+    gaussian_blur_node = Node(
+        package='camera_pipeline',
+        executable='gaussian_blur',
+        name='gaussian_blur',
+        output='screen',
+        remappings=[
+            ('image_raw', 'image_rect'),
+            ('output_image', 'image_blurred'),
+        ]
+    )
+
+    canny_edge_node = Node(
+        package='camera_pipeline',
+        executable='canny_edge',
+        name='canny_edge',
+        output='screen',
+        remappings=[
+            ('image_raw', 'image_blurred'),
+        ]
+    )
+
     return LaunchDescription([
-        rectify_node
+        rectify_node,
+        gaussian_blur_node,
+        canny_edge_node,
     ])
